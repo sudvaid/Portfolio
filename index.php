5c02be09930a407b5008a29a8f795d50e952b8d2
sudvaid <sudikshavaidy@gmail.com>
2020-04-13 16:12:28 -0700

    Added all files from Replit Portfolio

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+
+  <head>
+    <title>Sudiksha's Engineering Portfolio</title>
+  </head>
+
+  <body>
+
+  <?php echo  '<p>Hello world! This was made by Sudiksha Vaidyanathan, per 6. Here are all my links. </p>'; ?>
+  <ul>
+    <li><a href="index.html"> Home </a></li><br>
+    <li><a href="Last_AboutMe.html"> Vaidyanathan_AboutMe.html </a></li><br>
+    <li><a href="Last_Resume.html"> Vaidyanathan_Resume.html </a></li><br>
+      <li><a href="Last_Projects.html"> Vaidyanathan_Projects.html </a></li><br>
+     <li><a href="Last_ContactMe.html"> Vaidyanathan_ContactMe.html </a></li><br>
+  </ul>
+</body>
+
+
+</html>
